9f13fe16c8aa4c4102b0a0b47045627a2b8b59bf
tanersen3580 <tanersen@meb.gov.tr>
2022-07-23 22:39:22 +0300

    degistir fonksiyonu

---
+++ app/src/main/java/com/tanersen/mblapp1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.tanersen.mblapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun degistir (view : View){
+AdSoyad.text="Merhaba ben Android"
+        Dgm1.text="DÜĞMEYE BASILDI"
+
+    }
+}
